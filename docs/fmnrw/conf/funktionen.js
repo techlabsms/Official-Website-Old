204584ab90e05c2d4221eeaee05ece890ecaf205
Jannis Haendke <jhaendke@users.noreply.github.com>
2018-06-24 00:21:36 +0200

    added FM NRW handbook

---
+++ docs/fmnrw/conf/funktionen.js
@@ -0,0 +1,99 @@
+﻿/*Vereine & Steuern Version 9.0 Beta | Revision 150721*/
+
+/*GLOBALE VARIABLEN DEKLARIEREN*/
+var version  = 'Version 9.0 Beta';
+var revision = '| Revision 150721';
+
+/*FUNKTION FUSSZEILE
+--DER LINK ZU DIESER DATEI MUSS IM KOPF BEKANNT GEMACHT WERDEN !!!*/
+function fusszeile()
+{
+document.writeln("<p>Copyright 2015 Finanzministerium NRW</p>");
+document.writeln("<p><span>"+ version +"&nbsp;"+ revision +"</span></p>");
+}
+
+/*FUNKTION BREADCRUBS STARTSEITE
+--DIE GLOBALEN VARIABLEN WERDEN IN DER DATEI NOTIERT !!!*/
+function bc_start(){
+document.write("<a href=\"index.html\">Start</a>&nbsp;> <a href=\"./content/17/contentslist.html\">"+ kap +"</a>&nbsp; > "+ seite +" ");
+}
+
+/*FUNKTION BREADCRUBS
+--DIE GLOBALEN VARIABLEN WERDEN IN DER DATEI NOTIERT !!!*/
+function bc(){
+document.write("<a href=\"../../index.html\">Start</a>&nbsp;> <a href=\"index.html\">"+ kap +"</a>&nbsp; > "+ seite +" ");
+}
+
+/*FUNKTION BREADCRUBS
+--DIE GLOBALEN VARIABLEN WERDEN IN DER DATEI NOTIERT !!!*/
+function bc_eine(){
+document.write("<a href=\"../../index.html\">Start</a>&nbsp;> <a href=\"index.html\">"+ kap +"</a>&nbsp;  "+ seite +" ");
+}
+
+/*FUNKTION BREADCRUBS INHALTSÜBERSICHT
+--DIE GLOBALEN VARIABLEN WERDEN IN DER DATEI NOTIERT !!!*/
+function bc_sitemap(){
+document.write("<a href=\"../../index.html\">Start</a>&nbsp;> <a href=\"contentslist.html\">"+ kap +"</a>&nbsp;  "+ seite +" ");
+}
+
+/*FUNKTION SEITE VORWÄRTS UND RÜCKWÄRTS
+--DIE GLOBALEN VARIABLEN FÜR DIE LINKS WERDEN IN DER DATEI NOTIERT !!!*/
+function vz()
+{
+document.writeln("<a class=\"vz\"href=\""+ zur +"\" title=\"ein Beitrag zurück\">&lt; zurück</a><span class=\"trenn\">&nbsp;|&nbsp;</span><a class=\"vz\"href=\""+ vor +"\" title=\"Ein Beitrag weiter\">vor &gt;</a>");
+}
+
+/*FUNKTION iv() = INHALTSÜBERSICHT
+--RUFT IN ALLEN SEITEN DEN LINK UNTER DER NAVIGATION AUF !!!*/
+function iv()
+{
+document.writeln("<div class=\"link_to_contentslist\">");
+document.writeln("<img src=\"../images/arrow_contentslist_right.gif\" alt=\"\" width=\"16\" height=\"5\" />");
+document.writeln("<a href=\"../17/contentslist.html\">Inhaltsübersicht</a>");
+document.writeln("</div>");
+}
+
+/*FUNKTION logo_vs() = LOGO BEITRAGENDE
+DER FUNKTIONSAUFRUF FÜGT DAS LOGO FINANZVERWALTUNG EIN !!!*/
+function logo_vs()
+{
+document.writeln("<div class=\"vs_logo\"><img src=\"../../content/images/vs_logo.png\" alt=\"Ende Beitrag\" width=\"200\" height=\"32\" /></div>");
+}
+
+/*FUNKTION HEADER-BAR
+--RUFT DEN KOPF DER SEITE AUF !!!*/
+function h_bar()
+{
+document.writeln("<div class=\"heading_bar\">");
+document.writeln("<div class=\"manual_name\"><a href=\"../../index.html\">Vereine & Steuern</a></div>");
+document.writeln("<!--<div class=\"print\"><a href=\"javascript:void(0);\" target=\"_self\" onclick=\"openPrintWindow();\">Drucken</a></div>-->");
+document.writeln("<form name=\"suchen\" action=\"\" onSubmit=\"sende_eingabe(); return false\">");
+document.writeln("<div class=\"search_button\" onmouseout=\"this.className='search_button';\" onmouseover=\"this.className='search_button_mouseover';\">");
+document.writeln("<button id=\"id_search_button\" type=\"submit\">Suche</button>");
+document.writeln("</div>");
+document.writeln("<div class=\"search_input\">");
+document.writeln("<input type=\"text\" name=\"suchtext\" id=\"id_search\" value=\"\" size=\"\">");
+document.writeln("</div>");
+document.writeln("</form>");
+document.writeln("<div class=\"absender\"></div>");
+document.writeln("</div>");
+}
+
+/*FUNKTION HEADER-BAR STARTSEITE(INDEX)
+--RUFT DEN KOPF DER SEITE AUF !!!*/
+function h_bar_start()
+{
+document.writeln("<div class=\"heading_bar\">");
+document.writeln("<div class=\"manual_name\"><a href=\"index.html\">Vereine & Steuern</a></div>");
+document.writeln("<!--<div class=\"print\"><a href=\"javascript:void(0);\" target=\"_self\" onclick=\"openPrintWindow();\">Drucken</a></div>-->");
+document.writeln("<!--<form name=\"suchen\" action=\"\" onSubmit=\"sende_eingabe(); return false\">");
+document.writeln("<div class=\"search_button\" onmouseout=\"this.className='search_button';\" onmouseover=\"this.className='search_button_mouseover';\">");
+document.writeln("<button id=\"id_search_button\" type=\"submit\">Suche</button>");
+document.writeln("</div>");
+document.writeln("<div class=\"search_input\">");
+document.writeln("<input type=\"text\" name=\"suchtext\" id=\"id_search\" value=\"\" size=\"\">");
+document.writeln("</div>");
+document.writeln("</form>-->");
+document.writeln("<div class=\"absender\"></div>");
+document.writeln("</div>");
+}
---
+++ docs/fmnrw/conf/mainmenu.js
@@ -0,0 +1,53 @@
+﻿/* JAVASCRIPT ALS EXTERNE JS-DATEI MIT FUNKTIONSAUFRUF. */
+
+/*MENÜ FÜR DIE INHALTSSEITEN*/
+function MainMenu()
+{
+document.writeln("<ul class=\"mainmenu\">");
+document.writeln("<li><a href=\"../../content/90/90_01.html\">Benutzung</a></li>");
+document.writeln("<li><a href=\"../../content/01/index.html\">Gemeinnützigkeit</a></li>");
+document.writeln("<li><a href=\"../../content/02/index.html\">Spenden und Mitgliedsbeiträge</a></li>");
+document.writeln("<li><a href=\"../../content/03/index.html\">Körperschaftsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/04/04_01.html\">Gewerbesteuer</a></li>");
+document.writeln("<li><a href=\"../../content/05/index.html\">Umsatzsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/06/index.html\">Lohnsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/07/07_01.html\">Kapitalertragsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/08/08_01.html\">Steuerabzug für ausländische Künstler und Sportler</a></li>");
+document.writeln("<li><a href=\"../../content/09/09_01.html\">Steuerabzug bei Bauleistungen</a></li>");
+document.writeln("<li><a href=\"../../content/10/10_01.html\">Grundsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/11/11_01.html\">Grunderwerbsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/12/12_01.html\">Erbschaft- und Schenkungsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/13/13_01.html\">Kraftfahrzeugsteuer</a></li>");
+document.writeln("<li><a href=\"../../content/14/14_01.html\">Lotteriesteuer</a></li>");
+document.writeln("<li><a href=\"../../content/15/15_01.html\">Vordrucke</a></li>");
+document.writeln("<li><a href=\"../../content/16/index.html\">Anhang</a></li>");
+document.writeln("<li><a href=\"../../content/17/index.html\">Info-Hinweise</a></li>");
+document.writeln("<li><a href=\"../../content/17/17_05.html\">Impressum</a></li>");
+document.writeln("</ul>");
+}
+
+/*MENÜ FÜR DIE STARTSEITE*/
+function MainMenu_start()
+{
+document.writeln("<ul class=\"mainmenu\">");
+document.writeln("<li><a href=\"./content/90/90_01.html\">Benutzung</a></li>");
+document.writeln("<li><a href=\"./content/01/index.html\">Gemeinnützigkeit</a></li>");
+document.writeln("<li><a href=\"./content/02/index.html\">Spenden und Mitgliedsbeiträge</a></li>");
+document.writeln("<li><a href=\"./content/03/index.html\">Körperschaftsteuer</a></li>");
+document.writeln("<li><a href=\"./content/04/04_01.html\">Gewerbesteuer</a></li>");
+document.writeln("<li><a href=\"./content/05/index.html\">Umsatzsteuer</a></li>");
+document.writeln("<li><a href=\"./content/06/index.html\">Lohnsteuer</a></li>");
+document.writeln("<li><a href=\"./content/07/07_01.html\">Kapitalertragsteuer</a></li>");
+document.writeln("<li><a href=\"./content/08/08_01.html\">Steuerabzug für ausländische Künstler und Sportler</a></li>");
+document.writeln("<li><a href=\"./content/09/09_01.html\">Steuerabzug bei Bauleistungen</a></li>");
+document.writeln("<li><a href=\"./content/10/10_01.html\">Grundsteuer</a></li>");
+document.writeln("<li><a href=\"./content/11/11_01.html\">Grunderwerbsteuer</a></li>");
+document.writeln("<li><a href=\"./content/12/12_01.html\">Erbschaft- und Schenkungsteuer</a></li>");
+document.writeln("<li><a href=\"./content/13/13_01.html\">Kraftfahrzeugsteuer</a></li>");
+document.writeln("<li><a href=\"./content/14/14_01.html\">Lotteriesteuer</a></li>");
+document.writeln("<li><a href=\"./content/15/15_01.html\">Vordrucke</a></li>");
+document.writeln("<li><a href=\"./content/16/index.html\">Anhang</a></li>");
+document.writeln("<li><a href=\"./content/17/index.html\">Info-Hinweise</a></li>");
+document.writeln("<li><a href=\"./content/17/17_05.html\">Impressum</a></li>");
+document.writeln("</ul>");
+}
